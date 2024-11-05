2a0df8dee376e85cd1fb387d2fab55d2d30f1762
Anticripto/bicio <153502813+Orlanduis@users.noreply.github.com>
2024-11-04 23:49:58 -0700

    Update and rename WETH9.sol to AWETH9.sol

--- Contracts/AWETH9.sol
+++ Contracts/AWETH9.sol
@@ -31,8 +31,8 @@
 pragma solidity ^0.4.18;
 
 contract WETH9 {
-    string public name     = "Wrapped Ether";
-    string public symbol   = "WETH";
+    string public name     = "Anti Wrapped Eth";
+    string public symbol   = "AWETH";
     uint8  public decimals = 18;
 
     event  Approval(address indexed src, address indexed guy, uint wad);
